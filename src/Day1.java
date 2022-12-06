6915c75d43ae965221a431bf0dfb4bb6dbae9ef3
Brockbank <brockbam@amazon.com>
2022-12-06 18:20:15 -0500

    first attempt first git.

---
+++ src/Day1.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Day1 {
+
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new FileReader("inputs/Day1.txt"));
+        String line;
+        int runTotal = 0;
+
+        while ((line = br.readLine()) != null) {
+            String cal = line.trim();
+
+
+        }
+    }
+}
